44994ba5079aedef9e5b355db6490e9af07e911c
Mykhaylo Tymofyeyev <mykhaylo.tymofyeyev@deseip.com>
2023-06-08 17:09:21 +0200

    removed pwa

--- next.config.js
+++ next.config.js
@@ -1,19 +1,11 @@
 /** @type {import('next').NextConfig} */
 
-import withPWA from "next-pwa";
-import runtimeCaching from "next-pwa/cache.js";
-const isProduction = process.env.NODE_ENV === "production";
-
 const path = require("path");
 
-const nextConfig = withPWA({
-  dest: "public",
-  disable: !isProduction,
-  runtimeCaching,
-})({
+const nextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
   },
-});
+};
 
 module.exports = nextConfig;
